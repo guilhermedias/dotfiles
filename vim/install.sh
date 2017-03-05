56ac486da84aa08704791c76929d15584cb47977
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2017-03-05 02:56:26 -0300

    Move configuration files into a separate folder

--- vim/install.sh
+++ vim/install.sh
@@ -2,16 +2,16 @@ echo "Installing Vim"
 brew install vim
 
 echo "Linking Vim configuration files"
-ln -sf $PWD/vim/vimrc ~/.vimrc
+ln -sf $PWD/vim/config/vimrc ~/.vimrc
 
 mkdir -p ~/.vim/autoload
-ln -sf $PWD/vim/autoload/plug.vim ~/.vim/autoload/plug.vim
+ln -sf $PWD/vim/config/autoload/plug.vim ~/.vim/autoload/plug.vim
 
 mkdir -p ~/.vim/modules
-ln -sf $PWD/vim/modules/colors.vim ~/.vim/modules/colors.vim
+ln -sf $PWD/vim/config/modules/colors.vim ~/.vim/modules/colors.vim
 
 mkdir -p ~/.vim/colors
-ln -sf $PWD/vim/colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
+ln -sf $PWD/vim/config/colors/gruvbox.vim ~/.vim/colors/gruvbox.vim
 
 echo "Installing Vim plugins"
 vim +PlugClean +PlugInstall +qall
