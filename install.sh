669621d1018dcf158c2e05503cd798efeddc5f1d
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2017-03-05 12:07:30 -0300

    Improve Homebrew installation process

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env zsh
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install and update Homebrew
+if [ ! $(which brew) ]
+then
+  echo "Installing Homebrew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Brew already installed"
+fi
+
+echo "Updating Homebrew"
+brew update
+
+# Load configuration modules
 source vim/install.sh
